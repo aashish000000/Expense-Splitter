d1ac7ccd866a85c931441e8324fe8832c03dd73b
aashish000000 <ajoshi4@caldwell.edu>
2025-05-11 19:15:15 -0400

    patch

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.options('*', cors(corsOptions));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
